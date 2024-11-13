8e21f7b6f5619f844d3a5572ce6faf2283b786a3
herkouforyou01 <153167512+herkouforyou01@users.noreply.github.com>
2024-11-13 08:08:48 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "CmgjSLDD#mTy9MLskO4yb8YoU2R4RtQyYmJNpleHQQF-30uTiz-w",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
@@ -19,6 +19,6 @@ PREFIX: process.env.PREFIX || ".",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "true",
 OWNER_REACT: process.env.OWNER_REACT || "true",
-ALWAYS_OFFLINE: process.env.ALWAYS_OFFLINE || "true",
+ALWAYS_OFFLINE: process.env.ALWAYS_OFFLINE || "false",
 BOT_NAME: process.env.BOT_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
 };
